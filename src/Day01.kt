1edc799002c8af3b39a917ef404a89be8e8d91c5
TT <43578259+Lonexera@users.noreply.github.com>
2022-12-03 08:57:14 +0300

    add Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
+
+    fun List<String>.splitByEmpty(): List<List<String>> {
+        return buildList {
+            this@splitByEmpty.mapIndexedNotNull { index, string ->
+                if (string.isBlank()) {
+                    index
+                } else {
+                    null
+                }
+            }
+                    .zipWithNext() // 4,5    5,8   8,10  - no 0,4
+                    .forEachIndexed { index, (start, end) ->
+                        if (index == 0) {
+                            this.add(this@splitByEmpty.subList(0, start).toList())
+                        }
+
+                        this.add(this@splitByEmpty.subList(start + 1, end).toList())
+                    }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+                .splitByEmpty()
+                .maxOf { calories ->
+                    calories.sumOf { it.toInt() }
+                }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+                .splitByEmpty()
+                .map { calories ->
+                    calories.sumOf { it.toInt() }
+                }
+                .sortedDescending()
+                .take(3)
+                .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
