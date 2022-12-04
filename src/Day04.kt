9895dd97cdca406506d5f4b62f9e0013bd1bb5cd
TT <43578259+Lonexera@users.noreply.github.com>
2022-12-04 21:23:21 +0300

    add Day 4 + prepare day 5

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,11 +1,43 @@
 fun main() {
 
+    fun String.mapToElvesSections(): Pair<List<Int>, List<Int>> {
+        val sections = split(",")
+                .map { sections ->
+                    val (start, end) = sections
+                            .split("-")
+                            .map { it.toInt() }
+                            .zipWithNext()
+                            .first()
+
+                    (start..end).toList()
+                }
+
+        return sections[0] to sections[1]
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { elvesSection ->
+            val (firstSection, secondSection) = elvesSection.mapToElvesSections()
+
+            when {
+                firstSection.containsAll(secondSection) -> 1
+                secondSection.containsAll(firstSection) -> 1
+                else -> 0
+            }
+                    .toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { elvesSection ->
+            val (firstSection, secondSection) = elvesSection.mapToElvesSections()
+
+            when {
+                firstSection.any { secondSection.contains(it) } -> 1
+                else -> 0
+            }
+                    .toInt()
+        }
     }
 
     val input = readInput("Day04")
